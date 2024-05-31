90bc0b0d75be2b75587b8209203b869093dfa5b3
atifpasha44 <144209574+atifpasha44@users.noreply.github.com>
2024-05-31 23:43:07 +0530

    main'tf

---
+++ main.tf
@@ -0,0 +1,70 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "subnet" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+}
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        },
+      },
+    ],
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_ecs_cluster" "Container_Jenkins" {
+  name = "Container_Jenkins"
+}
+
+resource "aws_ecs_task_definition" "Container_Jenkins" {
+  family                   = "Container_Jenkins"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  memory                   = "512"
+  cpu                      = "256"
+
+  container_definitions = jsonencode([{
+    name  = "Container_Jenkins"
+    image = "nginx"
+    essential = true
+    portMappings = [{
+      containerPort = 80
+      hostPort      = 80
+    }]
+  }])
+}
+
+resource "aws_ecs_service" "Cluster_Jenkins" {
+  name            = "Cluster_Jenkins"
+  cluster         = aws_ecs_cluster.Container_Jenkins.id
+  task_definition = aws_ecs_task_definition.Container_Jenkins.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+  network_configuration {
+    subnets         = [aws_subnet.subnet.id]
+    assign_public_ip = true
+  }
+}
